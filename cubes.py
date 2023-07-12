8ad9428e4d49c0243fd208f2dc7c5558e60589f6
GeorgeM <georgemansbridge@msn.com>
2023-07-12 18:06:09 +0100

    split implementation into multiple files, add initial unit tests, add and rework parelism, add paralel logging

--- cubes.py
+++ cubes.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import math
 import argparse
 from time import perf_counter
 from libraries.cache import get_cache, save_cache, cache_exists
@@ -8,103 +9,131 @@ from libraries.packing import pack, unpack
 from libraries.parallel import dispatch_tasks, init_parrelism
 from libraries.renderer import render_shapes
 from libraries.rotation import all_rotations
+from multiprocessing import Queue
 
-def unpack_hashes_task(args):
+
+def unpack_hashes_task(args: tuple[list[int], Queue]) -> np.ndarray:
     cube_hashes, logging_queue = args
-    return [unpack(cube_hash) for cube_hash in cube_hashes]
-
-def hash_cubes_task(args):
-    base_cubes, logging_queue = args
-    # Empty list of new n-polycubes
-    polycubes = set()
+    polycubes = []
     uid = os.getpid()
 
     n = 0
+    log_base = max(math.ceil((len(cube_hashes)/ 1000)), 100) # send log updates every 0.1% or every 100, whichever is bigger
+    for cube_hash in cube_hashes:
+        polycubes.append(unpack(cube_hash))
+
+        if (n % log_base == 0):
+            if (logging_queue):
+                logging_queue.put((uid, n, len(cube_hashes)))
+            else:
+                print(f'done {((n/len(cube_hashes)) * 100):.2f} %')
+        n += 1
+
+    if (logging_queue):
+        logging_queue.put((uid, n, len(cube_hashes)))
+    else:
+        print(f'done {((n/len(cube_hashes)) * 100):.2f} %')
+    return polycubes
+
+
+def hash_cubes_task(args: tuple[list[np.ndarray], Queue]) -> list[int]:
+    base_cubes, logging_queue = args
+    # Empty list of new n-polycubes
+    hashes = set()
+    uid = os.getpid()
+
+    n = 0
+    log_base = max(math.ceil((len(base_cubes)/ 1000)), 100) # send log updates every 0.1% or every 100, whichever is bigger
     for base_cube in base_cubes:
         for new_cube in expand_cube(base_cube):
             cube_hash = get_canoincal_packing(new_cube)
-            polycubes.add(cube_hash)
-        if(n%1000 == 0):
-            if(logging_queue):
+            hashes.add(cube_hash)
+
+        if (n % log_base == 0):
+            if (logging_queue):
                 logging_queue.put((uid, n, len(base_cubes)))
             else:
                 print(f'done {((n/len(base_cubes)) * 100):.2f} %')
         n += 1
 
-    if(logging_queue):
+    if (logging_queue):
         logging_queue.put((uid, n, len(base_cubes)))
     else:
         print(f'done {((n/len(base_cubes)) * 100):.2f} %')
 
-    return polycubes
+    return hashes
 
-def generate_polycubes(n, use_cache=False, enable_multicore=False):
+
+def generate_polycubes(n: int, use_cache: bool = False, enable_multicore: bool = False) -> list[np.ndarray]:
     """
     Generates all polycubes of size n
-  
+
     Generates a list of all possible configurations of n cubes, where all cubes are connected via at least one face.
     Builds each new polycube from the previous set of polycubes n-1.
     Uses an optional cache to save and load polycubes of size n-1 for efficiency.
-  
+
     Parameters:
     n (int): The size of the polycubes to generate, e.g. all combinations of n=4 cubes.
-  
+
     Returns:
     list(np.array): Returns a list of all polycubes of size n as numpy byte arrays
-  
+
     """
     if n < 1:
         return []
     elif n == 1:
-        return [np.ones((1,1,1), dtype=np.byte)]
+        return [np.ones((1, 1, 1), dtype=np.byte)]
     elif n == 2:
-        return [np.ones((2,1,1), dtype=np.byte)]
+        return [np.ones((2, 1, 1), dtype=np.byte)]
 
-    if(use_cache and cache_exists(n)):
+    if (use_cache and cache_exists(n)):
         results = get_cache(n)
-        print(f"Got polycubes from cache n={n}")
+        print(f"Got polycubes from cache n={n}\n")
     else:
-        results = dispatch_tasks(hash_cubes_task, generate_polycubes(n-1, use_cache, enable_multicore), enable_multicore)
-        print(f"Hashed polycubes n={n}")
+        pollycubes = generate_polycubes(n-1, use_cache, enable_multicore)
+        results = dispatch_tasks(hash_cubes_task, pollycubes, enable_multicore)
+        print(f"Hashed polycubes n={n}\n")
         results = dispatch_tasks(unpack_hashes_task, results, enable_multicore)
-        print(f"Generated polycubes from hash n={n}")
-    
-    if(use_cache and not cache_exists(n)):
+        print(f"Generated polycubes from hash n={n}\n")
+
+    if (use_cache and not cache_exists(n)):
         save_cache(n, results)
 
     return results
 
-def get_canoincal_packing(polycube):
+
+def get_canoincal_packing(polycube: np.ndarray) -> int:
     """
     Determines if a polycube has already been seen.
-  
+
     Considers all possible rotations of a cube against the existing cubes stored in memory.
     Returns True if the cube exists, or False if it is new.
-  
+
     Parameters:
     polycube (np.array): 3D Numpy byte array where 1 values indicate polycube positions
-  
+
     Returns:
     boolean: True if polycube is already present in the set of all cubes so far.
     hash: the hash for this cube
-  
+
     """
     max_hash = 0
     for cube_rotation in all_rotations(polycube):
         this_hash = pack(cube_rotation)
-        if(this_hash > max_hash):
+        if (this_hash > max_hash):
             max_hash = this_hash
     return max_hash
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-                    prog='Polycube Generator',
-                    description='Generates all polycubes (combinations of cubes) of size n.')
+        prog='Polycube Generator',
+        description='Generates all polycubes (combinations of cubes) of size n.')
 
     parser.add_argument('n', metavar='N', type=int,
-                    help='The number of cubes within each polycube')
-    
-    #Requires python >=3.9
+                        help='The number of cubes within each polycube')
+
+    # Requires python >=3.9
     parser.add_argument('--cache', action=argparse.BooleanOptionalAction)
     parser.add_argument('--multicore', action=argparse.BooleanOptionalAction)
     parser.add_argument('--render', action=argparse.BooleanOptionalAction)
@@ -126,8 +155,8 @@ if __name__ == "__main__":
     # Stop the timer
     t1_stop = perf_counter()
 
-    if(render):
+    if (render):
         render_shapes(all_cubes, "./out")
 
-    print (f"\nFound {len(all_cubes)} unique polycubes")
-    print (f"\nElapsed time: {round(t1_stop - t1_start,3)}s")
+    print(f"\nFound {len(all_cubes)} unique polycubes")
+    print(f"\nElapsed time: {round(t1_stop - t1_start,3)}s")
--- libraries/packing.py
+++ libraries/packing.py
@@ -1,14 +1,15 @@
 import numpy as np
 
-def pack(polycube: np.ndarray):
+
+def pack(polycube: np.ndarray) -> int:
     """
     Converts a 3D ndarray into a single unsigned integer for quick hashing and efficient storage
 
     Converts a {0,1} nd array into a single unique large integer
-  
+
     Parameters:
     polycube (np.array): 3D Numpy byte array where 1 values indicate polycube positions
-  
+
     Returns:
     int: a unique integer hash
 
@@ -22,26 +23,27 @@ def pack(polycube: np.ndarray):
         cube_hash = (cube_hash << 8) + int(part)
     return cube_hash
 
-def unpack(cube_hash):
+
+def unpack(cube_hash: int) -> np.ndarray:
     """
     Converts a single integer back into a 3D ndarray
 
 
     Parameters:
     cube_hash (int): a unique integer hash
-  
+
     Returns:
     np.array: 3D Numpy byte array where 1 values indicate polycube positions
 
     """
     parts = []
-    while(cube_hash):
-        parts.append(cube_hash%256)
+    while (cube_hash):
+        parts.append(cube_hash % 256)
         cube_hash >>= 8
     parts = parts[::-1]
-    shape = (parts[0],parts[1],parts[2])
+    shape = (parts[0], parts[1], parts[2])
     data = parts[3:]
     size = shape[0] * shape[1] * shape[2]
     raw = np.unpackbits(np.array(data, dtype=np.uint8), bitorder='big')
-    final =  raw[0:size].reshape(shape)
+    final = raw[0:size].reshape(shape)
     return final
